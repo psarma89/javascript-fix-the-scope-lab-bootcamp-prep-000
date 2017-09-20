1311027dc66cadd7f1b0f29565ae97761c0b232f
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-20 15:20:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function yourAnimal() {
 function add2(n) {
   // Feel free to move things around!
   const two = 2
-    
+  //returns ends function execution, so has to be last statement
   return n + two
 }
 
